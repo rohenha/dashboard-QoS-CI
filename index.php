7dcb45bb91370960ff0216f6a336ccb9bca87375
yattlane <yattlane@92.222.127.190>
2016-11-29 16:39:34 +0100

    création page protet et home

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/master.css">
+    </head>
+    <body>
+        <?php require 'inc/header.php';?>
+        <?php require 'inc/pg_project.php';?>
+        <?php require 'inc/footer.php';?>
+
+    </body>
+</html>
+
+
+
+
+
+
