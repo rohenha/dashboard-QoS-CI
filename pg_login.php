c521e70492eff199534e45d224b422ea5ace0bbf
yattlane <yattlane@92.222.127.190>
2016-11-30 15:31:56 +0100

    modif page login

--- pg_login.php
+++ pg_login.php
@@ -5,9 +5,9 @@
         <div id="form_connexion">
             <h1>Page de connexion</h1>
             <form method="POST" action="">
-                <p><label>Identifiant</label><br><input type="text" name="login"></p>
-                <p><label>Mot de passe</label><br><input type="password" name="password"></p>
-                <p><input type="submit" value="Valider"/></p>
+                <p><label>Identifiant</label><input type="text" name="login"></p>
+                <p><label>Mot de passe</label><input type="password" name="password"></p>
+                <p><input type="submit" value="Valider" class="btn-red"/></p>
             </form>
         </div>
     </div>
