12263cebd7eccccac390779892482b0562922d89
yattlane <yattlane@92.222.127.190>
2016-11-30 16:37:58 +0100

    update

--- pg_login.php
+++ pg_login.php
@@ -4,10 +4,10 @@
     <div class="bloc_size">
         <div id="form_connexion">
             <h1>Page de connexion</h1>
-            <form method="POST" action="">
-                <p><label>Identifiant</label><input type="text" name="login"></p>
+            <form method="POST" autocomplete="off" action="/" >
+                <p><label>Identifiant</label><input type="text" name="login" ></p>
                 <p><label>Mot de passe</label><input type="password" name="password"></p>
-                <p><input type="submit" value="Valider" class="btn-red"/></p>
+                <p><input type="submit" value="Valider" class="btn-purple"/></p>
             </form>
         </div>
     </div>
