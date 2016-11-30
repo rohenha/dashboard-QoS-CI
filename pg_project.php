ee239a457e0ce7f4e0b5d2d072af48dff8b9f8f1
Romain Breton <romain.breton.pro@gmail.com>
2016-11-30 16:59:10 +0100

    Ajout des polices et avancement des animations

--- pg_project.php
+++ pg_project.php
@@ -1,15 +1,32 @@
 <?php include "inc/header.php" ?>
 
-<div class="container">
+<div class="container flex dirRow wrap">
 	<div class="block-container w25 wd33 wt50 wm100">
 		<div class="block square w100 gradient blue">
 			<div class="block-content  bigColumn flex dirCol aiC jcC wrap">
 					<span class="anim whiteSun large" class="bodymovin" data-animation-path="animations/" data-anim-loop="true" data-name="White Sun"></span>
-					<h3>PHP MD</h3>
 					<div class="notif-content">
 						<p class="status">Status :<span>Success</span></p>
 						<p>All users on MySpace will know that there are millions of people out there.</p>
 					</div>
+					<h3>PHP MD</h3>
+					<div class="percentage w100 flex dirCol aiS jcB">
+						<p>50%</p>
+						<span class="bar w50"></span>
+					</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="block-container w25 wd33 wt50 wm100">
+		<div class="block square w100 gradient yellow">
+			<div class="block-content  bigColumn flex dirCol aiC jcC wrap">
+					<span class="anim whiteSun large" class="bodymovin" data-animation-path="animations/" data-anim-loop="true" data-name="White Sun"></span>
+					<div class="notif-content">
+						<p class="status">Status :<span>Success</span></p>
+						<p>All users on MySpace will know that there are millions of people out there.</p>
+					</div>
+					<h3>PHP MD</h3>
 					<div class="percentage w100 flex dirCol aiS jcB">
 						<p>50%</p>
 						<span class="bar w50"></span>
