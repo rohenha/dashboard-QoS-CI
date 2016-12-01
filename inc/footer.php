a909841694c6a295f88b7c3ca85308e75c942ee6
yattlane <yattlane@92.222.127.190>
2016-12-01 09:34:36 +0100

    mise à jour page login

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,6 @@
 		<script src="js/jquery.min.js"></script>
 		<script src="js/bodymovin.js"></script>
 		<script src="js/app.js"></script>
+		<script src="js/functions.js"></script>
 	</body>
 </html>
--- pg_login.php
+++ pg_login.php
@@ -4,10 +4,11 @@
     <div class="bloc_size">
         <div id="form_connexion">
             <h1>Page de connexion</h1>
-            <form method="POST" autocomplete="off" action="/" >
-                <p><label>Identifiant</label><input type="text" name="login" ></p>
-                <p><label>Mot de passe</label><input type="password" name="password"></p>
-                <p><input type="submit" value="Valider" class="btn-purple"/></p>
+            <form  autocomplete="off" method="POST" action="/" >
+                <p><label>Identifiant</label><input type="text" name="login" value="" ></p>
+                <p><label>Mot de passe</label><input type="password" name="password" value=""></p>
+                <p><input type="submit" value="Valider" class="btn-purple bloc_right"/></p>
+                <p class="clear"></p>
             </form>
         </div>
     </div>
